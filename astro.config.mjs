7d8070fd78b4c1528584bc38e19962a6b8e8a2cd
Sam Kerr <sam.p.kerr@gmail.com>
2025-09-30 23:03:28 +0100

    Rename repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      site: 'https://SamKerr.github.io',
-      base: '',
+      site: 'https://samkerr.github.io',
 });
